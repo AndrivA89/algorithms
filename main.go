6de8fd3fde472b388633da48bff6697d8fab9c74
Иващенко Андрей <ext-andrey.ivashchenko@sbermarket.ru>
2022-11-28 15:24:50 +0300

    Add quick sort

--- main.go
+++ main.go
@@ -7,9 +7,17 @@ import (
 )
 
 func main() {
+	sorting.QuickSort([]int{3, 2, 4, -10, 33, 5, 4, -1, 9})
+
 	sorting.SelectSort(tools.InitArray(1000))
 	sorting.SelectSort(tools.InitArray(50000))
 
+	sorting.QuickSort(tools.InitArray(1000))
+	sorting.QuickSort(tools.InitArray(50000))
+	sorting.QuickSort(tools.InitArray(1000000))
+
 	recursion.Factorial(3)
 	recursion.Factorial(50)
+
+	recursion.SumOfNumbers(tools.InitArray(1000000))
 }
--- sorting/selecting.go
+++ sorting/selecting.go
@@ -9,10 +9,11 @@ func SelectSort(array []int) {
 	startWork := time.Now()
 	fmt.Printf("SELECT SORT - array size: %v\n", len(array))
 
-	countSwap := 0
+	countOperation := 0
 	for i := range array {
 		min := i
 		for j := i + 1; j < len(array); j++ {
+			countOperation++
 			if array[j] < array[min] {
 				min = j
 			}
@@ -20,11 +21,11 @@ func SelectSort(array []int) {
 
 		if min != i {
 			array[i], array[min] = array[min], array[i]
-			countSwap++
+			countOperation++
 		}
 	}
 
-	fmt.Printf("number of operations = %d\n", countSwap)
+	fmt.Printf("number of operations = %d\n", countOperation)
 	fmt.Printf("duration = %v ms (%.3v s)\n\n",
 		time.Since(startWork).Milliseconds(),
 		time.Since(startWork).Seconds())
